65838a5ea012e2eb6132e9c3fcd4981d2a74dfa4
Felix <xilon@gmx.net>
2024-01-07 14:03:48 +0100

    stundenplan works when selecting class

--- m135/Kompetenzen/C/js/script.js
+++ m135/Kompetenzen/C/js/script.js
@@ -1,3 +1,20 @@
+// This script is released to the public domain and may be used, modified and
+// distributed without restrictions. Attribution not necessary but appreciated.
+// Source: https://weeknumber.com/how-to/javascript
+
+// Returns the ISO week of the date.
+Date.prototype.getWeek = function() {
+    var date = new Date(this.getTime());
+    date.setHours(0, 0, 0, 0);
+    // Thursday in current week decides the year.
+    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+    // January 4 is always in week 1.
+    var week1 = new Date(date.getFullYear(), 0, 4);
+    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                          - 3 + (week1.getDay() + 6) % 7) / 7);
+  }
+
 async function getDataBeruf() {
     $.ajax({
         url: "http://sandbox.gibm.ch/berufe.php",
@@ -34,6 +51,13 @@ async function getDataKlasse() {
     });
 }
 
+function calculateDate() {
+    var date = new Date($("#date").val()); 
+    const week = date.getWeek();
+    const year = date.getFullYear();
+    return week + "-" + year;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     getDataBeruf();
     getDataKlasse();
@@ -54,5 +78,54 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     });
+    KlasseDropdown.addEventListener("change", (event) => {
+        $.ajax({
+            url: "http://sandbox.gibm.ch/tafel.php?klasse_id=" + event.target.value + "&woche=" + calculateDate(),
+            method: "GET",
+            dataType: "json",
+            success: function(response) {
     
+                $("#table").empty();
+                console.log(calculateDate());
+                console.log(response);
+
+                const table = $("<table>");
+                    const thead = $("<thead>");
+                    const tbody = $("<tbody>");
+                    
+                    const headerRow = $("<tr>");
+
+                    const headerNames = ["Datum", "Wochentag", "Von", "Bis", "Lehrer", "Fach", "Raum"];
+
+                    headerNames.forEach(header => {
+                        const th = $("<th>").text(header);
+                        headerRow.append(th);
+                    });
+
+                    thead.append(headerRow);
+                    table.append(thead);
+
+                $.each(response, function(index, rowData) {
+
+                    const row = $("<tr>");
+
+                    const headers = ["tafel_datum", "tafel_wochentag", "tafel_von", "tafel_bis", "tafel_lehrer", "tafel_longfach", "tafel_raum"];
+                    headers.forEach(header => {
+                        if (header == "tafel_wochentag") {
+                            //array of days
+                            const days = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
+                            row.append($("<td>").text(days[rowData[header]]));
+                        } else {
+                            const cell = $("<td>").text(rowData[header]);
+                            row.append(cell);
+                        }
+                    });
+                    tbody.append(row);
+                });
+
+                table.append(tbody);
+                $("#table").append(table);
+            }
+        });
+    });
 });
