2ed3a8e845af3b9c01d94d52278a2743eb5fe97d
Felix <xilon@gmx.net>
2024-01-07 16:25:16 +0100

    Stundenplan funktioniert immer, Webstorage speichern, bootstrap

--- m135/Kompetenzen/C/js/script.js
+++ m135/Kompetenzen/C/js/script.js
@@ -3,129 +3,256 @@
 // Source: https://weeknumber.com/how-to/javascript
 
 // Returns the ISO week of the date.
-Date.prototype.getWeek = function() {
-    var date = new Date(this.getTime());
-    date.setHours(0, 0, 0, 0);
-    // Thursday in current week decides the year.
-    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-    // January 4 is always in week 1.
-    var week1 = new Date(date.getFullYear(), 0, 4);
-    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
-                          - 3 + (week1.getDay() + 6) % 7) / 7);
-  }
+Date.prototype.getWeek = function () {
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return (
+    1 +
+    Math.round(
+      ((date.getTime() - week1.getTime()) / 86400000 -
+        3 +
+        ((week1.getDay() + 6) % 7)) /
+        7
+    )
+  );
+};
 
 async function getDataBeruf() {
-    $.ajax({
-        url: "http://sandbox.gibm.ch/berufe.php",
-        method: "GET",
-        dataType: "json",
-        success: function(response) {
-            const dropdown = $("#BerufsgruppeDropdown");
+  $.ajax({
+    url: "http://sandbox.gibm.ch/berufe.php",
+    method: "GET",
+    dataType: "json",
+    success: function (response) {
+      const dropdown = $("#BerufsgruppeDropdown");
 
-            dropdown.empty();
-
-            $.each(response, function(index, beruf) {
-                const option = $("<option>").val(beruf.beruf_id).text(beruf.beruf_name);
-                dropdown.append(option);
-            });
-        }
-    });
+      $.each(response, function (index, beruf) {
+        const option = $("<option>").val(beruf.beruf_id).text(beruf.beruf_name);
+        dropdown.append(option);
+      });
+    },
+  });
 }
 
 async function getDataKlasse() {
-    $.ajax({
-        url: "http://sandbox.gibm.ch/klassen.php",
-        method: "GET",
-        dataType: "json",
-        success: function(response) {
-            const dropdown = $("#KlasseDropdown");
+  $.ajax({
+    url: "http://sandbox.gibm.ch/klassen.php",
+    method: "GET",
+    dataType: "json",
+    success: function (response) {
+      const dropdown = $("#KlasseDropdown");
 
-            dropdown.empty();
-
-            $.each(response, function(index, klasse) {
-                const option = $("<option>").val(klasse.klasse_id).text(klasse.klasse_longname);
-                dropdown.append(option);
-            });
-        }
-    });
+      $.each(response, function (index, klasse) {
+        const option = $("<option>")
+          .val(klasse.klasse_id)
+          .text(klasse.klasse_longname);
+        dropdown.append(option);
+      });
+    },
+  });
 }
 
 function calculateDate() {
-    var date = new Date($("#date").val()); 
-    const week = date.getWeek();
-    const year = date.getFullYear();
-    return week + "-" + year;
+  var date = new Date($("#date").val());
+  const week = date.getWeek();
+  const year = date.getFullYear();
+  return week + "-" + year;
+}
+
+function updateTable(response) {
+  $("#table").empty();
+
+  const table = $("<table>");
+  const thead = $("<thead>");
+  const tbody = $("<tbody>");
+
+  const headerRow = $("<tr>");
+
+  const headerNames = [
+    "Datum",
+    "Wochentag",
+    "Von",
+    "Bis",
+    "Lehrer",
+    "Fach",
+    "Raum",
+  ];
+
+  headerNames.forEach((header) => {
+    const th = $("<th>").text(header);
+    headerRow.append(th);
+  });
+
+  thead.append(headerRow);
+  table.append(thead);
+
+  $.each(response, function (index, rowData) {
+    const row = $("<tr>");
+
+    const headers = [
+      "tafel_datum",
+      "tafel_wochentag",
+      "tafel_von",
+      "tafel_bis",
+      "tafel_lehrer",
+      "tafel_longfach",
+      "tafel_raum",
+    ];
+    headers.forEach((header) => {
+      if (header == "tafel_wochentag") {
+        //array of days
+        const days = [
+          "Sonntag",
+          "Montag",
+          "Dienstag",
+          "Mittwoch",
+          "Donnerstag",
+          "Freitag",
+          "Samstag",
+        ];
+        row.append($("<td>").text(days[rowData[header]]));
+      } else {
+        const cell = $("<td>").text(rowData[header]);
+        row.append(cell);
+      }
+    });
+    tbody.append(row);
+  });
+
+  table.append(tbody);
+  $("#table").append(table);
+
+  if ($("#KlasseDropdown").val() == 3518598) {
+    $("#table").empty();
+  } else if (response.length === 0) {
+    $("#table").empty();
+    showUpdateMessage("Letztes Update: " + new Date().toLocaleString());
+  } else {
+    showUpdateMessage("Letztes Update: " + new Date().toLocaleString());
+  }
+}
+
+function showUpdateMessage(message) {
+    const updateMessageElement = document.getElementById("updateMessage");
+    updateMessageElement.textContent = message;
+  }
+
+async function getDataWebstorage() {
+    const beruf_id = localStorage.getItem("beruf_id");
+    const beruf_name = localStorage.getItem("beruf_name");
+    const klasse_id = localStorage.getItem("klasse_id");
+    const klasse_longname = localStorage.getItem("klasse_longname");
+    const date = localStorage.getItem("date");
+    
+    if (beruf_id && beruf_name) {
+        $("#BerufsgruppeDropdown").val(beruf_id).text(beruf_name);
+    }
+    if (klasse_id && klasse_longname) {
+        $("#KlasseDropdown").val(klasse_id).text(klasse_longname);
+    }
+    if (date) {
+        $("#date").val(date);
+    }
+
+    if (klasse_id && date == null) {
+        $.ajax({
+            url:
+              "http://sandbox.gibm.ch/tafel.php?klasse_id=" +
+              klasse_id,
+            method: "GET",
+            dataType: "json",
+            success: function (response) {
+              updateTable(response);
+            },
+          });
+    }
+
+    if(klasse_id && date) {
+        $.ajax({
+            url:
+              "http://sandbox.gibm.ch/tafel.php?klasse_id=" +
+              klasse_id +
+              "&woche=" +
+              date,
+            method: "GET",
+            dataType: "json",
+            success: function (response) {
+              updateTable(response);
+            },
+          });
+    }
+
+    showUpdateMessage("Letztes Update: " + new Date().toLocaleString());
+
+    
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    getDataBeruf();
-    getDataKlasse();
-    BerufsgruppeDropdown.addEventListener("change", (event) => {
-        $.ajax({
-            url: "http://sandbox.gibm.ch/klassen.php?beruf_id=" + event.target.value,
-            method: "GET",
-            dataType: "json",
-            success: function(response) {
-                const dropdown = $("#KlasseDropdown");
-    
-                dropdown.empty();
-    
-                $.each(response, function(index, klasse) {
-                    const option = $("<option>").val(klasse.klasse_id).text(klasse.klasse_longname);
-                    dropdown.append(option);
-                });
-            }
+  getDataWebstorage();
+  getDataBeruf();
+  getDataKlasse();
+  showUpdateMessage("Letztes Update: " + new Date().toLocaleString());
+  BerufsgruppeDropdown.addEventListener("change", (event) => {
+    $.ajax({
+      url: "http://sandbox.gibm.ch/klassen.php?beruf_id=" + event.target.value,
+      method: "GET",
+      dataType: "json",
+      success: function (response) {
+        showUpdateMessage("Letztes Update: " + new Date().toLocaleString());
+
+        localStorage.setItem("beruf_id", event.target.value);
+        localStorage.setItem("beruf_name", event.target.options[event.target.selectedIndex].text);
+
+        const dropdown = $("#KlasseDropdown");
+
+        dropdown.empty();
+
+        $.each(response, function (index, klasse) {
+          const option = $("<option>")
+            .val(klasse.klasse_id)
+            .text(klasse.klasse_longname);
+          dropdown.append(option);
         });
+      },
     });
-    KlasseDropdown.addEventListener("change", (event) => {
-        $.ajax({
-            url: "http://sandbox.gibm.ch/tafel.php?klasse_id=" + event.target.value + "&woche=" + calculateDate(),
-            method: "GET",
-            dataType: "json",
-            success: function(response) {
-    
-                $("#table").empty();
-                console.log(calculateDate());
-                console.log(response);
+  });
+  KlasseDropdown.addEventListener("change", (event) => {
+    $.ajax({
+      url:
+        "http://sandbox.gibm.ch/tafel.php?klasse_id=" +
+        event.target.value +
+        "&woche=" +
+        calculateDate(),
+      method: "GET",
+      dataType: "json",
+      success: function (response) {
 
-                const table = $("<table>");
-                    const thead = $("<thead>");
-                    const tbody = $("<tbody>");
-                    
-                    const headerRow = $("<tr>");
+        localStorage.setItem("klasse_id", event.target.value);
+        localStorage.setItem("klasse_longname", event.target.options[event.target.selectedIndex].text);
 
-                    const headerNames = ["Datum", "Wochentag", "Von", "Bis", "Lehrer", "Fach", "Raum"];
-
-                    headerNames.forEach(header => {
-                        const th = $("<th>").text(header);
-                        headerRow.append(th);
-                    });
-
-                    thead.append(headerRow);
-                    table.append(thead);
-
-                $.each(response, function(index, rowData) {
-
-                    const row = $("<tr>");
-
-                    const headers = ["tafel_datum", "tafel_wochentag", "tafel_von", "tafel_bis", "tafel_lehrer", "tafel_longfach", "tafel_raum"];
-                    headers.forEach(header => {
-                        if (header == "tafel_wochentag") {
-                            //array of days
-                            const days = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
-                            row.append($("<td>").text(days[rowData[header]]));
-                        } else {
-                            const cell = $("<td>").text(rowData[header]);
-                            row.append(cell);
-                        }
-                    });
-                    tbody.append(row);
-                });
-
-                table.append(tbody);
-                $("#table").append(table);
-            }
-        });
+        updateTable(response);
+      },
     });
+  });
+  date.addEventListener("change", (event) => {
+    $.ajax({
+      url:
+        "http://sandbox.gibm.ch/tafel.php?klasse_id=" +
+        $("#KlasseDropdown").val() +
+        "&woche=" +
+        calculateDate(),
+      method: "GET",
+      dataType: "json",
+      success: function (response) {
+
+        localStorage.setItem("date", event.target.value);
+
+        updateTable(response);
+      },
+    });
+  });
 });
